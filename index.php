ba071696612b55978cfc77c85aa0dff938ab4676
Francisco Garcia <ciscog11@msn.com>
2016-04-14 13:51:33 -0600

    added comment to remind to change use case to ordered list

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 				<p>
 					Persona: The typical persona is someone looking for recipes that values production value. They would also appreciate a step by step guide of, not only written instructions, but step by step photo documentation as well.
 				</p>
-				<p>
+				<p><!-- change to ordered list 0) goal stated 1) specific, actionable steps for both the persona and site 3) goal accomplished -->
 					Use Case: The Persona wants to put time and effort into their next planned out, home cooked meal. They remember a soup that they had from an Italian Restaurant but don't necessarily like the Restaurant so aren't planning on going back. They search specifically for the soup and find a popular recipe on thecandidappetite blog. They go to the blog page and see the plentiful step by step recipe and photo journal. They end up making the recipe and enjoy it enough to want to add a reply of their enjoyment from making the soup in the comment section at the bottom of the blog page. In addition, since the Persona enjoyed using the recipe they noticed tags that let to other sections of recipes that they are interested in cooking in the future.
 				</p>
 		<hr>
