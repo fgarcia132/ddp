2386f7bb3b51fed0876d295c09b825b6f959e052
Francisco Garcia <ciscog11@msn.com>
2016-04-14 08:32:15 -0600

    added typical persona

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>The Candid Appetite</h1>
+			<h2>1. Front End</h2>
+				<p>
+					Persona: The typical persona is someone looking for recipes that values production value. They would also appreciate a step by step guide of, not only written instructions, but step by step photo documentation as well.
+				</p>
+	</body>
+</html>
