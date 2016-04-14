105780465aef8c821089b6ed39ffae450b5e080a
Francisco Garcia <ciscog11@msn.com>
2016-04-14 08:47:27 -0600

    added to use case

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 					Persona: The typical persona is someone looking for recipes that values production value. They would also appreciate a step by step guide of, not only written instructions, but step by step photo documentation as well.
 				</p>
 				<p>
-					Use Case
+					Use Case: The Persona wants to put time and effort into their next planned out, home cooked meal. They remember a soup that they had from an Italian Restaurant but don't necessarily like the Restaurant so aren't planning on going back. They search specifically for the soup and find a popular recipe on thecandidappetite blog. They go to the blog page and see the plentiful step by step recipe and photo journal. They end up making the recipe and enjoy it enough to want to add a reply of their enjoyment from making the soup in the comment section at the bottom of the blog page.
 				</p>
 	</body>
 </html>
