39df8fe29acce4ad5f64238ae0bf5dbc4c7e278f
Francisco Garcia <ciscog11@msn.com>
2016-04-18 09:15:39 -0600

    added .sql

---
+++ ddl.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS tag;
+
+CREATE TABLE recipe (
+        recipeId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+        recipeContent VARCHAR(5000) NOT NULL,
+        PRIMARY KEY(recipeId)
+);
+
+CREATE TABLE comment (
+        commentId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+        email VARCHAR(128) NOT NULL,
+        dateTime TIMESTAMP NOT NULL,
+        replyContent VARCHAR(1000) NOT NULL,
+        INDEX(recipeId),
+        FOREIGN KEY(recipeId) REFERENCES  recipe(recipeId),
+        PRIMARY KEY(commentId)
+);
+
+CREATE TABLE tag (
+        tagId INT UNSIGNED NOT NULL,
+        foodType VARCHAR(128) NOT NULL,
+        INDEX(recipeId),
+        FOREIGN KEY(recipeId) REFERENCES recipe(recipeId),
+        PRIMARY KEY(tagId)
+);
