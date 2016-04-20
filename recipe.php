65554fe505f22a65c5b37fd2c8dd5d571204be22
Francisco Garcia <ciscog11@msn.com>
2016-04-20 11:58:58 -0600

    added to recipe.php

--- recipe.php
+++ recipe.php
@@ -140,6 +140,110 @@ class Recipe implements \JsonSerializable {
 	 * inserts this Recipe into mySQL
 	 *
 	 * @param \PDO $pdo PDO connection object
-	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
 	 */
+	public function insert(\PDO $pdo) {
+		// enforce the recipeId is null (don't insert a recipe that already exists
+		if($this->recipeId !== null) {
+			throw(new \PDOException("not a new recipe"));
+		}
+
+		// create query template
+		$query = "INSERT INTO recipe(recipeContent, recipeDate) VALUES(:recipeContent, :recipeDate)";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template
+		$formattedDate = $this->recipeDate->format("Y-m-d H:i:s");
+		$parameters = ["recipeContent" => $this-> recipeContent, "recipeDate" => $formattedDate];
+		$statement->execute($parameters);
+
+		// update the null recipeId with what mySQL just gave us
+		$this->recipeId = intval($pdo->lastInsertId());
+	}
+
+	/**
+	 * deletes this Recipe from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function delete(\PDO $pdo) {
+		// enforce the recipeId is not null (i.e., don't delete a tweet that hasn't been inserted)
+		if($this->recipeId === null) {
+			throw(new \PDOException("unable to delete a tweet that does not exist"));
+		}
+
+		// create query template
+		$query = "DELETE FROM recipe WHERE recipeId = :reciepeId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holder in the template
+		$parameters = ["recipeId" => $this->recipeId];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * updates this recipe in mySQL
+	 *
+	 * @param \PDO $pdo PDO conencetion object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function update(\PDO $pdo) {
+		//enforce the recipeId is not null (i.e., don't update a tweet  that hasn't been inserted)
+		if($this->recipeId === null) {
+			throw(new \PDOException("unable to update a recipe that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE recipe SET recipeContent = : recipeContent, recipeDate = :recipeDate WHERE recipeId = :recipeId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holder in the template
+		$formattedDate = $this->recipeDate->format("Y-m-d H:i:s");
+		$parameters = ["recipeContent" => $this->recipeContent, "recipeDate" => $formattedDate, "recipeId" => $this->recipeId];
+		$statement->execute($parameters);
+	}
+	/**
+	 * gets the Recipe by content
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * $param strings $recipeContent recipe content to search for
+	 * @return \SpliFixedArray of Recipes found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data tppe
+	 */
+	public static function getRecipeByRecipeContent(\PDO $pdo, string $recipeContent) {
+		// sanitize the description before searching
+		$recipeContent = trim($recipeContent);
+		$recipeContent = filter_var($recipeContent, FILTER_SANITIZE_STRING);
+		if(empty($recipeContent) === true) {
+			throw(new \PDOException("recipe content is invalid"));
+		}
+
+		// create query template
+		$query = "SELECT recipeId, recipeContent, commentId, recipeDate FROM recipe WHERE recipeContent LIKE :recipeContent";
+		$statement = $pdo->prepare($query);
+
+		// bind the recipe content to the place holder in the template
+		$recipeContent = "%recipeContent%";
+		$parameters = array("recipeContent" => $recipeContent);
+		$statement->execute($parameters);
+
+		// build an array of recipes
+		$recipes = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$recipes - new Recipe($row["recipeId"], $row[recipeId], $row["recipeContent"], $row["recipeDate"]);
+				$recipes->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return($recipes);
+	}
 }
